5fe97e63af35c0b7cfe859186eb287ec0e4b33e6
adpriya21 <priyadarshiniad.20mec@jyotinivas.in>
2025-08-14 04:45:18 +0530

    07: Dashboard Navbar

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -7,13 +7,25 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
 
 export const DashboardUserButton = () => {
   const router = useRouter();
+  const isMobile = useIsMobile();
   const { data, isPending } = authClient.useSession();
 
   const onLogout = async () => {
@@ -29,9 +41,55 @@ export const DashboardUserButton = () => {
     return null;
   }
 
+  if (isMobile) {
+    return (
+      <Drawer>
+        <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
+          <div className="flex items-center gap-2">
+            {data.user.image ? (
+              <Avatar>
+                <AvatarImage src={data.user.image} alt={data.user.name} />
+                <AvatarFallback>
+                  {data.user.name?.charAt(0).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+            ) : (
+              <GeneratedAvatar
+                seed={data.user.name || "?"}
+                variant="initials" // or "botttsNeutral"
+                className="size-9"
+              />
+            )}
+            <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+              <p className="text-sm truncate w-full">{data.user.name}</p>
+              <p className="text-xs truncate w-full">{data.user.email}</p>
+            </div>
+          </div>
+          <ChevronDownIcon className="size-4 shrink-0" />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{data.user.name}</DrawerTitle>
+            <DrawerDescription>{data.user.email}</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button variant="outline" onClick={() => {}}>
+              <CreditCardIcon className="size-4 text-black" />
+              Billing
+            </Button>
+            <Button variant="outline" onClick={onLogout}>
+              <LogOutIcon className="size-4 text-black" />
+              Logout
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2">
         <div className="flex items-center gap-2">
           {data.user.image ? (
             <Avatar>
@@ -69,7 +127,10 @@ export const DashboardUserButton = () => {
           Billing
           <CreditCardIcon className="size-4 ml-auto" />
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={onLogout} className="cursor-pointer flex items-center justify-between">
+        <DropdownMenuItem
+          onClick={onLogout}
+          className="cursor-pointer flex items-center justify-between"
+        >
           Logout
           <LogOutIcon className="size-4 ml-auto" />
         </DropdownMenuItem>
